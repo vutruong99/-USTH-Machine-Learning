2c2fb344b71a7176ae44d6ead3aedcf7a50724c6
Thi <truongsithivu99@gmail.com>
2019-11-12 20:28:13 +0700

    BGD, Mini-batch, and Stochastic GD

--- grad_desc_multinput.py
+++ grad_desc_multinput.py
@@ -1,28 +1,134 @@
 import numpy as np
-import pandas as pd
-from sklearn import linear_model
+import matplotlib.pyplot as plt
 
-""" def gradient_descent(x,y):
-    current_a = 0  # Sometimes known as m
-    current_b = 0
-    learning_rate = 0.0001  # alpha
+
+def cost_f(x, y, theta):
     n = len(x)
-    iteration = 10
+    predictions = x.dot(theta)
+    cost = (1 / (2*n)) * np.sum(np.square(predictions - y))
+    return cost
+
+
+def stochastic(x, y):
+    theta = np.array([0, 0, 0, 0])
+    theta.shape += (1,)
+    n = len(x)
+    learning_rate = 0.00001  # alpha
+    iteration = 100
+
+    costs = np.zeros(iteration)  # Table of cost values after each iteration
+    thetas = np.zeros((iteration, 4))  # Table of theta values after each iteration
+
     for i in range(iteration):
-        y_predicted = current_a * x + current_b
-        cost = (1 / n) * sum([val ** 2 for val in (y - y_predicted)])
+        for j in range(n):
+            random_index = np.random.randint(0,n)
 
-        derivative_a = -(2/n) * sum(x*(y - y_predicted))
-        derivative_b = -(2/n) * sum(y - y_predicted)
+            x_r = x[random_index, :].reshape(1, x.shape[1])
+            y_r = y[random_index, :].reshape(1, 1)
 
-        current_a = current_a - learning_rate * derivative_a
-        current_b = current_b - learning_rate * derivative_b
+            prediction = np.dot(x_r, theta)
+            theta = theta - (1 / n) * learning_rate * (x_r.T.dot((prediction - y_r)))
+            thetas[i, :] = theta.T
+            cost = cost_f(x,y,theta)
+        costs[i] = cost
 
-        print("a = {}, b = {}, iteration = {}, cost = {}". format(current_a,current_b,i,cost)) """
+    print("STOCHASTIC GRADIENT DESCENT")
+    print("theta0 = {:0.5f}, theta1 = {:0.5f}, theta2 = {:0.5f}, theta3 = {:0.5f}".format(theta[0][-1], theta[1][-1],
+                                                                                          theta[2][-1], theta[3][-1]))
+    print("cost = {:0.5f}".format(costs[-1]))
+    print("")
+    return costs
 
 
-x = np.array([[30,3,6], [43,4,8], [25,2,3], [51,4,9], [40,3,5], [20,1,2]])
-y = np.array([2.5,3.4,1.8,4.5,3.2,1.6])
+def mini_batch(x, y):
+    batch_size = 2
+    theta = np.array([0, 0, 0, 0])
+    theta.shape += (1,)
+    n = len(x)
+    learning_rate = 0.00001  # alpha
+    iteration = 100
 
-theta = np.array([])
-print(x[1])
+    costs = np.zeros(iteration)  # Table of cost values after each iteration
+    thetas = np.zeros((iteration, 4))  # Table of theta values after each iteration
+
+    for i in range(iteration):
+        indexes = np.random.permutation(n)
+        x = x[indexes]
+        y = y[indexes]
+        for j in range(0, n, batch_size):
+            x_j = x[j:j+batch_size]
+            x_j = np.c_[np.ones(len(x_j)), x_j]  # Add one to the X matrix
+            y_j = y[j:j+batch_size]
+            prediction = np.dot(x_j, theta)
+            theta = theta - (1 / batch_size) * learning_rate * (x_j.T.dot((prediction - y_j)))
+            thetas[i, :] = theta.T
+
+        costs[i] = cost_f(np.c_[np.ones(len(x)),x],y,theta)
+
+    print("MINI BATCH GRADIENT DESCENT")
+    print("theta0 = {:0.5f}, theta1 = {:0.5f}, theta2 = {:0.5f}, theta3 = {:0.5f}".format(theta[0][-1], theta[1][-1], theta[2][-1], theta[3][-1]))
+    print("cost = {:0.5f}".format(costs[-1]))
+    print("")
+
+    return costs
+
+
+def gradient_descent(x, y):
+    theta = np.array([0, 0, 0, 0])
+    theta.shape += (1,)
+    n = len(x)
+    learning_rate = 0.00001  # alpha
+    iteration = 100
+
+    costs = np.zeros(iteration)  # Table of cost values after each iteration
+    thetas = np.zeros((iteration, 4))  # Table of theta values after each iteration
+
+    for i in range(iteration):
+        prediction = np.dot(x, theta)
+        theta = theta - (1 / n) * learning_rate * (x.T.dot((prediction - y)))
+        thetas[i, :] = theta.T
+        costs[i] = cost_f(x, y, theta)
+
+    print("BATCH GRADIENT DESCENT")
+    print("theta0 = {:0.5f}, theta1 = {:0.5f}, theta2 = {:0.5f}, theta3 = {:0.5f}".format(theta[0][-1], theta[1][-1], theta[2][-1], theta[3][-1]))
+    print("cost = {:0.5f}".format(costs[-1]))
+    print("")
+
+
+    return costs
+
+
+def normal_equation(x , y):
+    print("NORMAL EQUATION THETAS")
+    print(np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y))
+
+
+x = np.array([[30, 3, 6], [43, 4, 8], [25, 2, 3], [51, 4, 9], [40, 3, 5], [20, 1, 2]])
+x_oneAdded = np.c_[np.ones((len(x), 1)), x]
+
+y = np.array([2.5, 3.4, 1.8, 4.5, 3.2, 1.6])
+y.shape += (1,)
+
+costg = gradient_descent(x_oneAdded,y)
+costm = mini_batch(x,y)
+costs = stochastic(x_oneAdded,y)
+normal_equation(x_oneAdded,y)
+
+plt.xlabel("Epochs")
+plt.ylabel("Costs")
+plt.title("Costs after 100 epochs")
+plt.plot(np.linspace(1,100,num=100),costg,'b',label="Batch")
+plt.plot(np.linspace(1,100,num=100),costm,'g',label="Mini-batch")
+plt.plot(np.linspace(1,100,num=100),costs,'r',label="Stochastic")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
